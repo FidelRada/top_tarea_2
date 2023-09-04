9d24d43803370e5a67288991afab6fb82b400db4
fidelrada <fidel.rada.bo@gmail.com>
2023-09-04 17:52:26 -0400

    commit 22

--- api_flask.py
+++ api_flask.py
@@ -28,5 +28,5 @@ def receive_message():
     return "Mensaje recibido", 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80)
     
